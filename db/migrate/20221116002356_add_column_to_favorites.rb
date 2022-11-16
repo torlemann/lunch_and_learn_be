9c1f41bd33ee19a885c046148eaecdd609ae8015
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-11-15 18:20:22 -0800

    Rerun migration after sudo kill

---
+++ db/migrate/20221116002356_add_column_to_favorites.rb
@@ -0,0 +1,5 @@
+class AddColumnToFavorites < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :favorites, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_203742) do
+ActiveRecord::Schema.define(version: 2022_11_16_002356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_11_15_203742) do
     t.string "recipe_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2022_11_15_203742) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "users"
 end
