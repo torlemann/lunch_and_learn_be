73b88aae632b0d0dadca85170874351ad3d4e505
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-11-15 19:30:49 -0800

    Refactor favorite request with sad paths

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::FavoritesController < ApplicationController
     def index
+      user = User.find_by(api_key: params[:api_key])
+      if user.nil?
+        render json: { error: "Unable to find user by API key. A valid API key must be provided to create favorite."}, status: 400 
+      else
+        favorites = Favorite.where(api_key: params[:api_key])
+      end
+
       favorites = Favorite.where(api_key: params[:api_key])
       render json:(FavoritesSerializer.get_format(favorites))
     end
--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'favorites request spec' do
   describe "post" do
-    it 'creates favorites', :vcr do 
-      VCR.eject_cassette
-      VCR.turn_off!
+    it 'happy path creates favorites with valid api key', :vcr do 
+      # VCR.eject_cassette
+      # VCR.turn_off!
       user_params = { 
         email: "smoochy@gmail.com",
         name: "Robin"
@@ -28,10 +28,28 @@ RSpec.describe 'favorites request spec' do
       expect(response).to be_successful
       expect(response.status).to eq 201
     end
+
+    it 'sad path returns error with invalid api key', :vcr do 
+
+      favorites_params = {
+        "api_key": "kjadoiu398479t8yghubdjkvnsjr932",
+        "country": "thailand",
+        "recipe_link": "https://www.tastingtable.com/.....",
+        "recipe_title": "Crab Fried Rice (Khaao Pad Bpu)"
+      }
+
+      post '/api/v1/favorites', params: favorites_params
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq 400
+      expect(json[:error]).to eq("Unable to find user by API key. A valid API key must be provided to create favorite.")
+    end
   end
 
   describe "get" do
-    it "returns all favorites with specified api key" do
+    it "happy path returns all favorites with specified api key", :vcr do
       user_params = { 
         email: "smoochy@gmail.com",
         name: "Robin"
@@ -70,7 +88,42 @@ RSpec.describe 'favorites request spec' do
       json = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
+      expect(json).to be_a Hash
       expect(json).to have_key :data
+      expect(json[:data]).to be_an Array
+      expect(json[:data].count).to eq 2
+      expect(json[:data][0]).to be_a Hash
+      expect(json[:data][0].keys).to eq([:id, :type, :attributes])
+      expect(json[:data][0][:id]).to be_an Integer
+      expect(json[:data][0][:type]).to eq("favorite")
+      expect(json[:data][0][:attributes].keys).to eq([:country, :recipe_title, :recipe_link, :created_at])   
+      expect(json[:data][0][:attributes][:country]).to be_a String
+      expect(json[:data][0][:attributes][:recipe_link]).to be_a String
+      expect(json[:data][0][:attributes][:recipe_title]).to be_a String
+      expect(json[:data][0][:attributes][:created_at]).to be_a String
+    end
+
+    it "sad path returns error with invalid api key", :vcr do
+      user_params = { 
+        email: "smoochy@gmail.com",
+        name: "Robin"
+      }
+
+      post '/api/v1/users',  params: user_params
+
+      user = User.last
+
+      favorites_request_params = {
+        "api_key": user.api_key
+      }
+
+      get '/api/v1/favorites', params: favorites_request_params
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(json).to be_a Hash
+      expect(json[:data]).to eq([])
     end
   end
 end
